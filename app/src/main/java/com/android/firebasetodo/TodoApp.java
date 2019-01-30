fe3a3c54df88d05203c0c4fd7431af52bd0478dc
Vengatesh M <vengateshm.13@gmail.com>
2019-01-30 17:38:46 +0530

    firebase database persistence enabled

--- app/src/main/java/com/android/firebasetodo/TodoApp.java
+++ app/src/main/java/com/android/firebasetodo/TodoApp.java
@@ -2,9 +2,13 @@ package com.android.firebasetodo;
 
 import android.app.Application;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class TodoApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 }
